77ba3ccd1f0e7b839af9d76d6207b01ebf4b368a
Nehal pradhan <40754890+nehalpradhan23@users.noreply.github.com>
2024-04-23 21:34:14 +0530

    message

---
+++ components/Footer.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="text-white-800 flex-between body-text w-full gap-y-10 border-t border-black-400 bg-black-100 px-20 py-1 max-md:flex-col">
+      <p>Copyright ©️ 2024 Nehal pradhan | All Rights Reserved</p>
+      <div className="flex gap-x-9">
+        <Link href="/terms-of-use">Terms & conditions</Link>
+        <Link href="/privacy-policy">Privacy policy</Link>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ components/Navbar.tsx
@@ -0,0 +1,36 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <nav className="flex-center fixed top-0 z-50 w-full border-b-2 border-black-200 py-7 text-white">
+      <div className="flex-between mx-auto w-full max-w-screen-2xl px-6 xs:px-8 sm:px-16">
+        <Link href="/">
+          <Image src="/jsm-logo.svg" alt="logo" width={55} height={40} />
+        </Link>
+        {/* mobile menu icon ========= */}
+        <Image
+          src="/hamburger-menu.svg"
+          alt="menu"
+          width={30}
+          height={30}
+          className="block md:hidden"
+        />
+        <ul className="flex-center gap-x-3 max-md:hidden md:gap-x-10">
+          <li className="body-text text-gradient_blue-purple !font-bold">
+            <Link href="/" target="_blank">
+              Link 1
+            </Link>
+          </li>
+          <li className="body-text !font-normal">
+            <Link href="/" target="_blank">
+              Link 2
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
